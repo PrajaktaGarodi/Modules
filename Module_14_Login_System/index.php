35153da524e812580e38b6e155b7da615f5d8815
PrajaktaGarodi <prajaktagarodi@gmail.com>
2024-10-18 11:40:33 +0530

    update login form successfully....

---
+++ Module_14_Login_System/index.php
@@ -0,0 +1,61 @@
+<?php
+include('db_connection.php');
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<style>
+    .container-fluid
+    {
+        background-color: lightblue;
+        font-family: 'Times New Roman', Times, serif;
+        height: 100vh;
+        width: 100%;
+
+    }
+</style>
+
+<body>
+
+    <div class="container-fluid ">
+        <H1 class="text-center pt-5 pb-5">LOGIN FORM</H1>
+            <div class="row justify-content-center">
+                <div class="col-md-4 p-5 rounded-5 " style="background-color:whitesmoke;">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 mt-2">
+                            <div class="form-group">
+                                <label for="Username" class=" form-label text-center">USER NAME </label>
+                                <input type="text" class="form-control" name="username">
+                            </div>
+                        </div>
+                        <div class="col-md-12 mt-2">
+                        <div class="form-group">
+                                <label for="Password" class=" form-label text-center">PASSWORD </label>
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-3 mt-3">
+                            <button class="btn btn-secondary " type="submit" name="submit">SUBMIT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+
+
+</body>
+
+</html>
